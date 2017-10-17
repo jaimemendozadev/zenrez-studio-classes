e8f841106d934c24afcf92ef0ac72c4f12ed0cc3
jaimemendozadev <jaimemendozadev@gmail.com>
2017-10-17 06:25:36 -0700

    Start setting up Redux

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import ReduxPromise from 'redux-promise';
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Path} from 'react-router-dom';
 
 
-const App = () => {
-  return(
-    <h1>Welcome to the Zenrez Studio Class Schedule!</h1>
-  )
-}
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <h1>Welcome to the Zenrez Studio Class Schedule!</h1>
+    </div>
+  </BrowserRouter>, document.querySelector('.container'));
