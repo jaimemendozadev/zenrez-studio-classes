10a6036d19f67acaccea9a65aa663b9aead1faac
jaimemendozadev <jaimemendozadev@gmail.com>
2017-10-18 16:50:02 -0700

    Add procfile and change PORT number

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const Router = require('./Router');
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(Router);
 
+const PORT = proces.env.PORT || 3000;
 
-app.listen(3000, ()=> {
+app.listen(PORT, ()=> {
   console.log("Listening on port 3000");
 })
