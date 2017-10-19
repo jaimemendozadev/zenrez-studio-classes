8026c18292c775aa172458bed2de6b28a084cdfc
jaimemendozadev <jaimemendozadev@gmail.com>
2017-10-18 17:13:39 -0700

    Change Router routes

--- server/Router/index.js
+++ server/Router/index.js
@@ -1,14 +1,14 @@
 const Router = require('express').Router();
 const path = require('path');
 
-console.log("the dirname in Router is", __dirname);
+console.log("the dirname in Router is", __dirname); // /app/server/Router
 
 Router.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../../public', 'index.html'));
 });
   
 Router.get('/show/:id', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../../public', 'index.html'));
 });
 
 module.exports = Router;
